9793904b3b08fdd573993c4f6758e786b303eb00
BlanchonMarc <fr.marc.blanchon@gmail.com>
2019-06-13 07:42:44 +0200

    newton pep8

--- lib/newton.py
+++ lib/newton.py
@@ -8,6 +8,8 @@ def newton_polynomial(raw):
 
     R = np.zeros((m, n, 4))
 
+    # print(R)
+
     # Labeling different polatiration channels
     O = np.zeros((m, n), dtype=int)
 
@@ -41,100 +43,121 @@ def newton_polynomial(raw):
                 0.4375 * I[i, j - 1] + 0.4375 * I[i, j + 1] - \
                 0.25 * I[i, j + 2] + 0.0625 * I[i, j + 3]
             R[i, j, O[i + 1, j]] = 0.5 * I[i, j] + 0.0625 * I[i - 3, j] - 0.25 * I[i - 2, j] + \
-                0.4375*I[i-1, j] + 0.4375*I[i+1, j] - \
-                0.25*I[i+2, j] + 0.0625*I[i+3, j]
-            Y1[i, j] = 0.5*I[i, j] + 0.0625*I[i-3, j-3] - 0.25*I[i-2, j-2] + 0.4375 * \
-                I[i-1, j-1] + 0.4375*I[i+1, j+1] - \
-                0.25*I[i+2, j+2] + 0.0625*I[i+3, j+3]
-            Y2[i, j] = 0.5*I[i, j] + 0.0625*I[i-3, j+3] - 0.25*I[i-2, j+2] + 0.4375 * \
-                I[i-1, j+1] + 0.4375*I[i+1, j-1] - \
-                0.25*I[i+2, j-2] + 0.0625*I[i+3, j-3]
+                0.4375 * I[i - 1, j] + 0.4375 * I[i + 1, j] - \
+                0.25 * I[i + 2, j] + 0.0625 * I[i + 3, j]
+            Y1[i, j] = 0.5 * I[i, j] + 0.0625 * I[i - 3, j - 3] - 0.25 * I[i - 2, j - 2] + 0.4375 * \
+                I[i - 1, j - 1] + 0.4375 * I[i + 1, j + 1] - 0.25 * \
+                I[i + 2, j + 2] + 0.0625 * I[i + 3, j + 3]
+            Y2[i, j] = 0.5 * I[i, j] + 0.0625 * I[i - 3, j + 3] - 0.25 * I[i - 2, j + 2] + 0.4375 * \
+                I[i - 1, j + 1] + 0.4375 * I[i + 1, j - 1] - 0.25 * \
+                I[i + 2, j - 2] + 0.0625 * I[i + 3, j - 3]
     # One can adjust for better result
     thao = 2.8
     # Fusion of the estimations with edge classifier for case Fig.6(a).
 
-    for i in range(3, m-3):
-        for j in range(3, n-3):
+    for i in range(3, m - 3):
+        for j in range(3, n - 3):
             pha1 = 0.0
             pha2 = 0.0
 
             for k in range(-2, 3, 2):
                 for l in range(-2, 3, 2):
-                    pha1 = pha1 + abs(Y1[i+k, j+l] - I[i+k, j+l])
-                    pha2 = pha2 + abs(Y2[i+k, j+l] - I[i+k, j+l])
+                    pha1 = pha1 + \
+                        abs(Y1[i + k, j + l] -
+                            I[i + k, j + l])
+                    pha2 = pha2 + \
+                        abs(Y2[i + k, j + l] -
+                            I[i + k, j + l])
 
             if (pha1 / pha2) > thao:
-                R[i, j, O[i+1, j+1]] = Y2[i, j]
-            elif (pha2/pha1) > thao:
-                R[i, j, O[i+1, j+1]] = Y1[i, j]
-            elif (((pha1/pha2) < thao) and ((pha2/pha1) < thao)):
-                d1 = abs(I[i-1, j-1] - I[i+1, j+1]) + \
-                    abs(2*I[i, j] - I[i-2, j-2] - I[i+2, j+2])
-                d2 = abs(I[i+1, j-1] - I[i-1, j+1]) + \
-                    abs(2*I[i, j] - I[i+2, j-2] - I[i-2, j+2])
+                R[i, j, O[i + 1, j + 1]] = Y2[i, j]
+            elif (pha2 / pha1) > thao:
+                R[i, j, O[i + 1, j + 1]] = Y1[i, j]
+            elif (((pha1 / pha2) < thao) and ((pha2 / pha1) < thao)):
+                d1 = abs(I[i - 1, j - 1] - I[i + 1, j + 1]) + \
+                    abs(2 * I[i, j] - I[i - 2,
+                                        j - 2] - I[i + 2, j + 2])
+                d2 = abs(I[i + 1, j - 1] - I[i - 1, j + 1]) + \
+                    abs(2 * I[i, j] - I[i + 2,
+                                        j - 2] - I[i - 2, j + 2])
                 epsl = 0.000000000000001
-                w1 = 1/(d1 + epsl)
-                w2 = 1/(d2+epsl)
-                R[i, j, O[i+1, j+1]] = (w1*Y1[i, j] + w2*Y2[i, j])/(w1 + w2)
+                w1 = 1 / (d1 + epsl)
+                w2 = 1 / (d2 + epsl)
+                R[i, j, O[i + 1, j + 1]
+                  ] = (w1 * Y1[i, j] + w2 * Y2[i, j]) / (w1 + w2)
 
-    RR = np.array(R,dtype=np.double)
+    RR = np.array(R, dtype=np.double)
 
-    XX1 = np.array(raw,dtype=np.double)
-    XX2 = np.array(raw,dtype=np.double)
-    YY1 = np.array(raw,dtype=np.double)
-    YY2 = np.array(raw,dtype=np.double)
+    XX1 = np.array(raw, dtype=np.double)
+    XX2 = np.array(raw, dtype=np.double)
+    YY1 = np.array(raw, dtype=np.double)
+    YY2 = np.array(raw, dtype=np.double)
 
     # Stage two interpolation: interpolate horizontally for case Fig.6(b),
     # interpolate vertically for case Fig.6(c).
 
-    for i in range(3, m-3):
-        for j in range(3, n-3):
-            XX1[i, j] = R[i, j, O[i, j+1]]
-            XX2[i, j] = 0.5*I[i, j] + 0.0625 * \
-                R[i-3, j, O[i, j+1]] - 0.25*I[i-2, j]
+    for i in range(3, m - 3):
+        for j in range(3, n - 3):
+            XX1[i, j] = R[i, j, O[i, j + 1]]
+            XX2[i, j] = 0.5 * I[i, j] + 0.0625 * \
+                R[i - 3, j, O[i, j + 1]] - \
+                0.25 * I[i - 2, j]
             XX2[i, j] = XX2[i, j] + 0.4375 * \
-                R[i-1, j, O[i, j+1]] + 0.4375*R[i+1, j, O[i, j+1]]
-            XX2[i, j] = XX2[i, j] - 0.25*I[i+2, j] + 0.0625*R[i+3, j, O[i, j+1]]
-            YY1[i, j] = R[i, j, O[i+1, j]]
-            YY2[i, j] = 0.5*I[i, j] + 0.0625 * \
-                R[i, j-3, O[i+1, j]] - 0.25*I[i, j-2]
+                R[i - 1, j, O[i, j + 1]] + 0.4375 * \
+                R[i + 1, j, O[i, j + 1]]
+            XX2[i, j] = XX2[i, j] - 0.25 * I[i + 2, j] + \
+                0.0625 * R[i + 3, j, O[i, j + 1]]
+            YY1[i, j] = R[i, j, O[i + 1, j]]
+            YY2[i, j] = 0.5 * I[i, j] + 0.0625 * \
+                R[i, j - 3, O[i + 1, j]] - \
+                0.25 * I[i, j - 2]
             YY2[i, j] = YY2[i, j] + 0.4375 * \
-                R[i, j-1, O[i+1, j]] + 0.4375*R[i, j+1, O[i+1, j]]
-            YY2[i, j] = YY2[i, j] - 0.25*I[i, j+2] + 0.0625*R[i, j+3, O[i+1, j]]
+                R[i, j - 1, O[i + 1, j]] + \
+                0.4375 * R[i, j + 1, O[i + 1, j]]
+            YY2[i, j] = YY2[i, j] - 0.25 * I[i, j + 2] + \
+                0.0625 * R[i, j + 3, O[i + 1, j]]
 
-    # Fusion of the estimations with edge classifier for case Fig.6(b) and Fig.6(c).
+    # Fusion of the estimations with edge classifier
 
-    for i in range(3, m-4):
-        for j in range(3, n-4):
+    for i in range(3, m - 4):
+        for j in range(3, n - 4):
             pha1 = 0.0
             pha2 = 0.0
 
             for k in range(-2, 3, 2):
                 for l in range(-2, 3, 2):
-                    pha1 = pha1 + abs(XX1[i+k, j+l] - I[i+k, j+l])
-                    pha2 = pha2 + abs(XX2[i+k, j+l] - I[i+k, j+l])
+                    pha1 = pha1 + \
+                        abs(XX1[i + k, j + l] - I[i + k, j + l])
+                    pha2 = pha2 + \
+                        abs(XX2[i + k, j + l] - I[i + k, j + l])
 
             if (pha1 / pha2) > thao:
-                R[i, j, O[i, j+1]] = XX2[i, j]
-            elif (pha2/pha1) > thao:
-                R[i, j, O[i, j+1]] = XX1[i, j]
-            elif (((pha1/pha2) < thao) and ((pha2/pha1) < thao)):
-                d1 = abs(I[i, j-1] - I[i, j+1]) + \
-                    abs(2*I[i, j] - I[i, j-2] - I[i, j+2])
-                d2 = abs(I[i+1, j] - I[i-1, j]) + \
-                    abs(2 * I[i, j] - I[i + 2, j] - I[i - 2, j])
+                R[i, j, O[i, j + 1]] = XX2[i, j]
+            elif (pha2 / pha1) > thao:
+                R[i, j, O[i, j + 1]] = XX1[i, j]
+            elif (((pha1 / pha2) < thao) and ((pha2 / pha1) < thao)):
+                d1 = abs(I[i, j - 1] - I[i, j + 1]) + \
+                    abs(2 * I[i, j] - I[i, j - 2] - I[i, j + 2])
+                d2 = abs(I[i + 1, j] - I[i - 1, j]) + \
+                    abs(2 * I[i, j] -
+                        I[i + 2, j] - I[i - 2, j])
                 epsl = 0.000000000000001
                 w1 = 1 / (d1 + epsl)
                 w2 = 1 / (d2 + epsl)
-                R[i, j, O[i, j + 1]] = (w1 * XX1[i, j] + w2 * XX2[i, j]) / (w1 + w2)
+                R[i, j, O[i, j + 1]
+                  ] = (w1 * XX1[i, j] + w2 * XX2[i, j]) / (w1 + w2)
 
             pha1 = 0.0
             pha2 = 0.0
 
             for k in range(-2, 3, 2):
                 for l in range(-2, 3, 2):
-                    pha1 = pha1 + abs(YY1[i + k, j + l] - I[i + k, j + l])
-                    pha2 = pha2 + abs(YY2[i + k, j + l] - I[i + k, j + l])
+                    pha1 = pha1 + \
+                        abs(YY1[i + k, j + l] -
+                            I[i + k, j + l])
+                    pha2 = pha2 + \
+                        abs(YY2[i + k, j + l] -
+                            I[i + k, j + l])
 
             if (pha1 / pha2) > thao:
                 R[i, j, O[i + 1, j]] = YY2[i, j]
@@ -142,13 +165,16 @@ def newton_polynomial(raw):
                 R[i, j, O[i + 1, j]] = YY1[i, j]
             elif (((pha1 / pha2) < thao) and ((pha2 / pha1) < thao)):
                 d1 = abs(I[i, j - 1] - I[i, j + 1]) + \
-                    abs(2 * I[i, j] - I[i, j - 2] - I[i, j + 2])
+                    abs(2 * I[i, j] -
+                        I[i, j - 2] - I[i, j + 2])
                 d2 = abs(I[i + 1, j] - I[i - 1, j]) + \
-                    abs(2 * I[i, j] - I[i + 2, j] - I[i - 2, j])
+                    abs(2 * I[i, j] -
+                        I[i + 2, j] - I[i - 2, j])
                 epsl = 0.000000000000001
                 w1 = 1 / (d1 + epsl)
                 w2 = 1 / (d2 + epsl)
-                R[i, j, O[i, j + 1]] = (w1 * YY1[i, j] + w2 * YY2[i, j]) / (w1 + w2)
+                R[i, j, O[i, j + 1]
+                  ] = (w1 * YY1[i, j] + w2 * YY2[i, j]) / (w1 + w2)
 
     R = np.array(RR, dtype=np.double)
     I0 = np.array(R[:, :, 0], dtype=np.double)
