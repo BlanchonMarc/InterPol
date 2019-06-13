83075b829bf1299f2e378b8201ef5b168955cb79
BlanchonMarc <fr.marc.blanchon@gmail.com>
2019-06-13 16:29:35 +0200

    adding saving procedure

---
+++ utils/organizer.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.misc import imsave
+import os
+
+
+# Create test images from interpolation to test metrics benchmarking
+def image_saver(obj, f_name):
+    path_gt = 'benchmark/groundtruth/'
+    if f_name.endswith('/'):
+        f_name = f_name[:-1]
+    if not os.path.exists(f'{path_gt}{fname}'):
+        try:
+            os.makedirs(f'{path_gt}{fname}')
+            os.makedirs(f'{path_gt}{fname}/Intensity')
+            os.makedirs(f'{path_gt}{fname}/Stokes')
+        except FileExistsError:
+            # directory already exists
+            pass
+
+    names = ['i0.png', 'i45.png', 'i90.png', 'i135.png']
+
+    if not os.path.exists(f'{path_gt}{fname}/Intensity'):
+        try:
+            os.makedirs(f'{path_gt}{fname}/Intensity')
+        except FileExistsError:
+            # directory already exists
+            pass
+
+    for i in range(obj.images.shape[0]):
+        imsave(f'{path_gt}{f_name}/Intensity/{names[i]}',
+               np.array(obj.images[i, :, :], dtype=np.float))
+
+    imsave(f'{path_gt}{f_name}/rgb_aop.png', obj.rgb_aop())
+
+    imsave(f'{path_gt}{f_name}/rgb_aop.png', obj.rgb_pola())
+
+    names = ['s0.png', 's1.png', 's2.png']
+
+    if not os.path.exists(f'{path_gt}{fname}/Stokes'):
+        try:
+            os.makedirs(f'{path_gt}{fname}/Stokes')
+        except FileExistsError:
+            # directory already exists
+            pass
+
+    for i in range(obj.stokes.shape[0]):
+        imsave(
+            f'{path_gt}{f_name}/Stokes/{names[i]}',
+            obj.stokes[i, :, :])
+
+    imsave(f'{path_gt}{f_name}/I.png', obj.inte)
+    imsave(f'{path_gt}{f_name}/AoP.png', obj.aop)
+    imsave(f'{path_gt}{f_name}/DoP.png', obj.dop)
--- utils/test_images.py
+++ utils/test_images.py
@@ -7,8 +7,8 @@ from scipy.misc import imsave
 def create_test_img(obj):
     names = ['i0.png', 'i45.png', 'i90.png', 'i135.png']
     for i in range(obj.images.shape[0]):
-        imsave(f'benchmark/groundtruth/test/Intensity/{names[i]}', np.array(
-            obj.images[i, :, :], dtype=np.float))
+        imsave(f'benchmark/groundtruth/test/Intensity/{names[i]}',
+               np.array(obj.images[i, :, :], dtype=np.float))
 
     imsave(f'benchmark/groundtruth/test/rgb_aop.png', obj.rgb_aop())
 
@@ -17,7 +17,8 @@ def create_test_img(obj):
     names = ['s0.png', 's1.png', 's2.png']
     for i in range(obj.stokes.shape[0]):
         imsave(
-            f'benchmark/groundtruth/test/Stokes/{names[i]}', obj.stokes[i, :, :])
+            f'benchmark/groundtruth/test/Stokes/{names[i]}',
+            obj.stokes[i, :, :])
 
     imsave(f'benchmark/groundtruth/test/I.png', obj.inte)
     imsave(f'benchmark/groundtruth/test/AoP.png', obj.aop)
